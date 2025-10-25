76b06ebc2c12b4fb2486b49015c139dad9b178a8
shadxwkxw <lolkek2468@gmail.com>
2025-10-25 14:21:09 +0300

    --add 50/50 dashboard

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,9 +1,47 @@
-const Dashboard = () => {
-    return (
+import cl from '../styles/dashboard.module.css';
+import users from '../assets/users.svg';
+import cursor from '../assets/cursor.svg';
+import clock from '../assets/clockDef.svg';
+import stroke from '../assets/stroke.svg';
+import iconMap from '../assets/iconMap.svg'
+
+const stats = [
+    { title: 'Всего клиентов', value: 0, containerClass: cl.greenContainer, img: users },
+    { title: 'Общая дистанция', value: '0 км', containerClass: cl.blueContainer, img: cursor },
+    { title: 'Общее время', value: '0ч 0м', containerClass: cl.orangeContainer, img: clock },
+    { title: 'Общее время', value: '10:10', containerClass: cl.violetContainer, img: stroke },
+]
+
+const StatCard = ({ title, value, containerClass, img }) => (
+    <div className={cl.cardStat}>
         <div>
-            Панель управления
+            <p className={cl.allClients}>{title}</p>
+            <p className={cl.textClients}>{value}</p>
         </div>
-    )
-}
+        <div className={containerClass}>
+            <img className={cl.imgClients} src={img} />
+        </div>
+    </div>
+)
+
+const Dashboard = () => (
+    <div className={cl.container}>
+        <div className={cl.card}>
+            <h1 className={cl.titleCard}>Панель управления</h1>
+            <p className={cl.textCard}>Обзор маршрутов на сегодня</p>
+        </div>
+        {stats.map((stat, index) => (
+            <StatCard key={index} {...stat} />
+        ))}
+        <div className={cl.containerNoRoute}>
+            <div className={cl.greenBorderContainer}>
+                <img src={iconMap} className={cl.icon} />
+                <p className={cl.noRouteText}>Маршрут еще не создан</p>
+                <p className={cl.grayText}>Перейдите в Планировщик маршрутов, чтобы загрузить список клиентов и создать оптимальный маршрут.</p>
+                <p className={cl.greenText}>Начните планирование прямо сейчас</p>
+            </div>
+        </div>
+    </div>
+)
 
 export default Dashboard
